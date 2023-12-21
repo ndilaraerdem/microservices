4994954f8397886e39f50869952ac5042bfc6f33
Dilara Erdem <ndilaraerdem@gmail.com>
2023-12-21 13:23:49 +0300

    Initial commit

---
+++ rental-service/src/test/java/com/etiya/rentalservice/RentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etiya.rentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
